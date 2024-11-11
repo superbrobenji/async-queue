edb932398b43b815cd07479648d9a1235c5b4c38
Benjamin Swanepeol <49689582+superbrobenji@users.noreply.github.com>
2024-11-11 21:17:11 +0200

    switched to bun, added d.ts files

--- build.js
+++ build.js
@@ -1,26 +1,20 @@
-import { build } from "esbuild";
-import { dtsPlugin } from "esbuild-plugin-d.ts";
-
 const option = {
-    bundle: true,
-    color: true,
-    logLevel: "info",
-    sourcemap: true,
     entryPoints: ["./src/index.js"],
+    outdir: "./dist",
+    target: "node",
+    sourcemap: "linked",
     minify: true,
+    drop: ["console"],
 };
 (async () => {
-    await build({
+    await Bun.build({
         format: "esm",
-        outdir: "dist",
-        splitting: true,
-        plugins: [dtsPlugin()],
         ...option,
     }).catch(() => process.exit(1));
 
-    await build({
+    await Bun.build({
         format: "cjs",
-        outfile: "./dist/cjs.js",
+        naming: "cjs.js",
         ...option,
     }).catch(() => process.exit(1));
 })();
